4fbbba1efb5ae64f01ef954455bd24325d25b505
Yasmine Elnadi <yasmine_adel@hotmail.com>
2023-06-20 02:26:04 -0700

    tenuer done

--- src/components/TenureSelect.js
+++ src/components/TenureSelect.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 
-const TenureSelect = () => {
+const TenureSelect = ({ data, setData }) => {
+  const handleChange = (event) => {
+    setData({
+      ...data,
+      loanTerm: event.target.value,
+    });
+  };
   return (
-    <div>
-      TenureSelect
-    </div>
+    <FormControl fullWidth>
+      <InputLabel id="demo-simple-select-label">Age</InputLabel>
+      <Select
+        labelId="demo-simple-select-label"
+        id="demo-simple-select"
+        value={data.loanTerm}
+        label="Tenure"
+        onChange={handleChange}
+      >
+        <MenuItem value={5}>5 Years</MenuItem>
+        <MenuItem value={10}>10 Years</MenuItem>
+        <MenuItem value={20}>20 Years</MenuItem>
+        <MenuItem value={30}>30 Years</MenuItem>
+
+      </Select>
+    </FormControl>
   );
-}
+};
 
 export default TenureSelect;
