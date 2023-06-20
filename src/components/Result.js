3be6b5b3debf95dc22dae8e4dba62d2f18af3219
Yasmine Elnadi <yasmine_adel@hotmail.com>
2023-06-20 02:59:01 -0700

    done

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,4 +1,9 @@
 import React from "react";
+import { Stack, Typography } from "@mui/material";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Pie } from "react-chartjs-2";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 const Result = ({ data }) => {
   const { downPayment, homeValue, loanAmount, loanTerm, interestRate } = data;
@@ -11,8 +16,29 @@ const Result = ({ data }) => {
       (1 + interestPerMonth) ** totalLoanMonths) /
     ((1 + interestPerMonth) ** totalLoanMonths - 1);
 
-  
-  return <div>Result</div>;
+  const totalInterestGenerated = monthlyPayment * totalLoanMonths - loanAmount;
+
+  const pieChartData = {
+    labels: ["Principle", "Interest"],
+    datasets: [
+      {
+        label: "Ratio of Principle and Interest",
+        data: [homeValue, totalInterestGenerated],
+        backgroundColor: ["rgba(255, 99, 132, 0.2)", "rgba(54, 162, 235, 0.2)"],
+        borderColor: ["rgba(255, 99, 132, 1)", "rgba(54, 162, 235, 1)"],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  return (
+    <Stack gap={2}>
+      <Typography textAlign={"center"}>
+        Monthly Payment: $ {monthlyPayment.toFixed(2)}
+      </Typography>
+      <Pie data={pieChartData} />
+    </Stack>
+  );
 };
 
 export default Result;
